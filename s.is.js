8fbc4cdb83ef58b92dbf2e7052aae5da23a3f6c5
Mike Acton <macton@gmail.com>
2015-06-22 14:58:20 -0700

    removing use, so need to add curry

--- s.is.js
+++ s.is.js
@@ -1,17 +1,17 @@
 var S = require('s');
 
 var is = {
-  eq     : function( a, b ) { return a == b; },
-  exact  : function( a, b ) { return a === b; },
-  nor    : function( a, b ) { return !(a||b); },
-  nand   : function( a, b ) { return !(a&&b); },
-  not    : function( a )    { return !a; },
-  and    : function( a, b ) { return a&&b; },
-  lte    : function( a, b ) { return a <= b; },
-  gte    : function( a, b ) { return a >= b; },
-  gt     : function( a, b ) { return a > b; },
-  lt     : function( a, b ) { return a < b; },
-  neq    : function( a, b ) { return a != b; },
+  eq     : S.curry( function( a, b ) { return a == b; } ),
+  exact  : S.curry( function( a, b ) { return a === b; } ),
+  nor    : S.curry( function( a, b ) { return !(a||b); } ),
+  nand   : S.curry( function( a, b ) { return !(a&&b); } ),
+  not    : function( a ) { return !a; },
+  and    : S.curry( function( a, b ) { return a&&b; } ),
+  lte    : S.curry( function( a, b ) { return a <= b; } ),
+  gte    : S.curry( function( a, b ) { return a >= b; } ),
+  gt     : S.curry( function( a, b ) { return a > b; } ),
+  lt     : S.curry( function( a, b ) { return a < b; } ),
+  neq    : S.curry( function( a, b ) { return a != b; } ),
 };
 
 exports = module.exports = is;
